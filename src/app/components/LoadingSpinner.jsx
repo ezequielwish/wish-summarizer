d06608e2e3938883674e2fd7c51c1222e19fa009
Ezequiel Almeida <ezequielalmeida.wish@gmail.com>
2025-09-28 10:26:23 -0300

    add Clear Textarea button and loading spinner

---
+++ src/app/components/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+import "./LoadingSpinner.css";
+
+export default function LoadingSpinner() {
+    return (
+        <div className="loading-wrapper">
+            <div className="spinner"></div>
+        </div>
+    );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,12 +3,14 @@
 import styles from "./page.module.css";
 import { FaGithub } from "react-icons/fa";
 import { useState } from "react";
+import LoadingSpinner from "./components/LoadingSpinner.jsx";
 
 export default function Home() {
     const [summarizedWishes, setSummarizedWishes] = useState("");
-
+    const [loading, setLoading] = useState(false);
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setLoading(true);
         setSummarizedWishes("");
         const formData = new FormData(e.target);
         const wishes = formData.get("wishes");
@@ -21,9 +23,17 @@ export default function Home() {
             });
             const data = await response.json();
             setSummarizedWishes(data.summary);
+            setLoading(false);
         } catch (error) {
             console.error("Error summarizing wishes:", error);
         }
+    };
+
+    const handleClear = async (e) => {
+        e.preventDefault();
+        setSummarizedWishes("");
+        setLoading(false);
+        document.querySelector("textarea").value = "";
     };
 
     return (
@@ -37,19 +47,32 @@ export default function Home() {
                         className={styles.textarea}
                         required
                     ></textarea>
-                    <button type="submit" className={styles.button}>
-                        Resumir
-                    </button>
-                </form>
-                <section className={styles.results} style={{ display: summarizedWishes !== "" ? "block" : "none" }}>
-                    <h2 className={styles.resultsTitle}>Texto resumido:</h2> 
-                    <div
-                        id="resultsContainer"
-                        className={styles.resultsContainer}
-                    >
-                        <p>{summarizedWishes}</p>
+                    <div className={styles.buttonContainer}>
+                        <button type="submit" className={styles.summaryButton}>
+                            Resumir
+                        </button>
+                        <button
+                            type="button"
+                            className={styles.cleanButton}
+                            onClick={handleClear}
+                        >
+                            limpar
+                        </button>
                     </div>
-                </section>
+                </form>
+
+                {!loading || !summarizedWishes == "" ? (
+                    <section className={styles.results} style={{ display: summarizedWishes !== "" ? "block" : "none" }}>
+                        <h2 className={styles.resultsTitle}>Texto resumido:</h2>
+                        <div className={styles.resultsContainer}>
+                            <p>{summarizedWishes}</p>
+                        </div>
+                    </section>
+                ) : (
+                    <section className={styles.results}>
+                        <LoadingSpinner />
+                    </section>
+                )}
             </main>
             <footer className={styles.footer}>
                 <span className={styles.footerText}>
